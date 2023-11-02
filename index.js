d7aba25b97706daab4fb984bb45c01f92c98e416
marcospaulopaiva <marcos.poo@gmail.com>
2023-11-02 12:14:50 -0300

    funcao pegaArquivo() em modo assincrono - opcao 2

--- index.js
+++ index.js
@@ -6,11 +6,26 @@ function trataErro(erro){
     throw new Error(chalk.red(erro.code,'não há arquivo no diretorio'));
 }
 
-function pegaArquivo(caminhoDoArquivo){
-    const encoding = 'utf-8';
-    fs.promises.readFile(caminhoDoArquivo, encoding)
-      .then((texto) => console.log(chalk.green(texto)))
-      .catch(trataErro) //.catch((erro) => trataErro(erro))
+// promises com async/await
+
+async function pegaArquivo(caminhoDoArquivo){
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo,encoding);
+        console.log(chalk.green(texto));    
+    } catch (erro) {
+        trataErro(erro);
+    }
 }
+
+// promises com then()
+
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.promises
+//       .readFile(caminhoDoArquivo, encoding)
+//       .then((texto) => console.log(chalk.green(texto)))
+//       .catch(trataErro) //.catch((erro) => trataErro(erro))
+// }
 
 pegaArquivo('./arquivos/texto.md');
