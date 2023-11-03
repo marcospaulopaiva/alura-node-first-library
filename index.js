1dc72df7461f84631ba481bd07124a5f8e08a407
marcospaulopaiva <marcos.poo@gmail.com>
2023-11-03 15:20:24 -0300

    Trabalhando com regex

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+
+function extraiLinks(texto){
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: [captura[2]]}))
+    return resultados;
+}
+
 function trataErro(erro){
     console.log(erro);
     throw new Error(chalk.red(erro.code,'não há arquivo no diretorio'));
 }
 
-// promises com async/await
-
 async function pegaArquivo(caminhoDoArquivo){
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo,encoding);
-        console.log(chalk.green(texto));    
+        console.log(extraiLinks(texto));  
     } catch (erro) {
         trataErro(erro);
     } finally {
@@ -20,15 +26,5 @@ async function pegaArquivo(caminhoDoArquivo){
     }
 
 }
-
-// promises com then()
-
-// function pegaArquivo(caminhoDoArquivo){
-//     const encoding = 'utf-8';
-//     fs.promises
-//       .readFile(caminhoDoArquivo, encoding)
-//       .then((texto) => console.log(chalk.green(texto)))
-//       .catch(trataErro) //.catch((erro) => trataErro(erro))
-// }
 
 pegaArquivo('./arquivos/texto.md');
