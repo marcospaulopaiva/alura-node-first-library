2612e7bd4aa0c2ecb2a2888a04aab7e2e3121641
marcospaulopaiva <marcos.poo@gmail.com>
2023-11-06 20:48:30 -0300

    Usando a linha de comando.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function extraiLinks(texto){
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
     const capturas = [...texto.matchAll(regex)];
     const resultados = capturas.map(captura => ({[captura[1]]: [captura[2]]}))
-    return resultados;
+    return resultados.length !== 0 ? resultados : 'não há links no arquivo';
 }
 
 function trataErro(erro){
@@ -14,17 +14,15 @@ function trataErro(erro){
     throw new Error(chalk.red(erro.code,'não há arquivo no diretorio'));
 }
 
+
 async function pegaArquivo(caminhoDoArquivo){
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo,encoding);
-        console.log(extraiLinks(texto));  
+        return extraiLinks(texto);  
     } catch (erro) {
         trataErro(erro);
-    } finally {
-        console.log(chalk.yellow('operação concluída'));
-    }
-
+    } 
 }
 
-pegaArquivo('./arquivos/texto.md');
+export default pegaArquivo;
